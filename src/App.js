0a67642339b33c2ef07fa06d1425085cb85f5aaa
ashraf rahil <skashraf97@gmail.com>
2022-05-10 09:46:23 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 
 import './App.css';
+import Header from './components/Header/Header';
+import SimpleBottomNavigation from './components/MainNav';
 
 function App() {
   return (
-    <div className="App">
-    hello world
-    </div>
+    <>
+    
+  <Header />
+  <div className="App">  </div>
+    <SimpleBottomNavigation />
+    </>
   );
 }
 
