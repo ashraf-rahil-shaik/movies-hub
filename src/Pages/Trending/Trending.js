0b1201702bd90b334e8166f0c4b1f7c1c175e032
ashraf rahil <skashraf97@gmail.com>
2022-05-11 22:33:32 +0530

    fetched api in trending

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -1,11 +1,30 @@
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Outlet } from 'react-router-dom'
+import axios from 'axios'
+import SingleContent from '../../components/SingleContent/SingleContent';
 
 const Trending = () => {
+const [content, setContent] = useState([]);
+  const fetchTrending = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`
+    );
+    console.log(data);
+
+    setContent(data.results);
+  }
+  useEffect(()=>{
+    fetchTrending()
+  },[]);
   return (
     <div>
 <span className='pageTitle'>Trending</span>
+<div className ='trending'>{
+  content && content.map((c)=>(
+    <SingleContent key={c.id} />
+  ))
+}</div>
 <Outlet />
     </div>
 
