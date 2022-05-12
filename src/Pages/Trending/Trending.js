4c309afd9d1eaa9ec811c2f8d6e38c3bada1209c
ashraf rahil <skashraf97@gmail.com>
2022-05-12 13:02:09 +0530

    added posters and title in trending

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -10,7 +10,7 @@ const [content, setContent] = useState([]);
     const { data } = await axios.get(
       `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`
     );
-    console.log(data);
+    console.log(data.results);
 
     setContent(data.results);
   }
@@ -22,7 +22,11 @@ const [content, setContent] = useState([]);
 <span className='pageTitle'>Trending</span>
 <div className ='trending'>{
   content && content.map((c)=>(
-    <SingleContent key={c.id} />
+    <SingleContent key={c.id} id = {c.id} poster={c.poster_path} title={c.title}
+    date={c.frist_air_date}
+    media_type={c.media_type}
+    vote_average = {c.vote_average}
+    />
   ))
 }</div>
 <Outlet />
--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -1,9 +1,16 @@
 import React from 'react'
-
-const SingleContent = () => {
+import { img_300 ,unavailable} from '../../Config/Config'
+const SingleContent = ({id,poster,title,date,media_type,vote_average}) => {
   return (
-    <div>hello </div>
-  )
+
+
+  <div>
+<img src={poster?`${img_300}/${poster}`:unavailable} alt={title} />
+<b className='title'>{title}</b>
+<span>{media_type === "tv" ? "TV Series" :"Movie"} </span>
+</div>
+
+    )
 }
 
 export default SingleContent
