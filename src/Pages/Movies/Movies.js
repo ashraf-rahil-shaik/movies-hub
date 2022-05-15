01a6b9e90854035123d2697bf5159ec6ca8a9092
ashraf rahil <skashraf97@gmail.com>
2022-05-15 09:57:41 +0530

    added number of pages in pagination

--- src/Pages/Movies/Movies.js
+++ src/Pages/Movies/Movies.js
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { Outlet } from 'react-router-dom'
 import SingleContent from '../../components/SingleContent/SingleContent'
-import Genres from '../../components/Genres/Genres'
+// import Genres from '../../components/Genres/Genres'
 import CustomPagination from '../../components/Pagination/CustomPagination'
 const Movies = () => {
   const [page,setPage] =useState(1)
   const [content, setContent] = useState([]);
+  const [numOfPages,setNumOfPages] = useState()
   const fetchMovie = async () => {
     const { data } = await axios.get(
       `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
@@ -14,6 +15,7 @@ const Movies = () => {
     
 
     setContent(data.results);
+    setNumOfPages(data.total_pages);
   }
   useEffect(()=>{
 fetchMovie()
@@ -22,8 +24,8 @@ fetchMovie()
   return (
     <div>
    
-        <span className='pageTitle'>movies </span>
-        <Genres  />
+        <span className='pageTitle'>Movies </span>
+        {/* <Genres  /> */}
         <div className ='trending'>{
   content && content.map((c)=>(
     <SingleContent key={c.id} id = {c.id} poster={c.poster_path} title={c.title||c.name}
@@ -33,7 +35,7 @@ fetchMovie()
     />
   ))
 }</div>
-     <CustomPagination setPage={setPage} />
+     <CustomPagination setPage={setPage} numOfPages={numOfPages} />
         <Outlet /> 
     </div>
   )
